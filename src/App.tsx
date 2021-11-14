d8f7732f201edc17a31db7627f15eedcf06077db
Johan Naudin <jnaudin@gmail.com>
2021-11-14 19:11:39 +0100

    feature/adds graphql codegen

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { gql, useQuery } from "@apollo/client";
-
-const query = gql`
-  query($id: Int) {
-    # Define which variables will be used in the query (id)
-    Media(id: $id, type: ANIME) {
-      # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-      id
-      title {
-        romaji
-        english
-        native
-      }
-    }
-  }
-`;
-
-const variables = {
-  id: 15125,
-};
+import { useGetGenresQuery } from "./generated/graphql";
 
 function App() {
-  const { loading, error, data } = useQuery(query, { variables });
+  const { loading, error, data: genresData } = useGetGenresQuery();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
-    console.log(data)
+  console.log(genresData);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Manga List</header>
     </div>
   );
 }
