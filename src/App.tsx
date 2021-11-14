d7dfbae76c0b9e84d820550c2465620eb3309daf
Johan Naudin <jnaudin@gmail.com>
2021-11-14 10:10:26 +0100

    feature/implements first query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { gql, useQuery } from "@apollo/client";
+
+const query = gql`
+  query($id: Int) {
+    # Define which variables will be used in the query (id)
+    Media(id: $id, type: ANIME) {
+      # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+      id
+      title {
+        romaji
+        english
+        native
+      }
+    }
+  }
+`;
+
+const variables = {
+  id: 15125,
+};
 
 function App() {
+  const { loading, error, data } = useQuery(query, { variables });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+    console.log(data)
+
   return (
     <div className="App">
       <header className="App-header">
